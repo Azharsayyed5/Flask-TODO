11c55a62fcbfe25afbf834eba5a0b6537a78e1c6
AZHAR SAYYED <55562412+Azharsayyed5@users.noreply.github.com>
2020-12-18 20:28:13 +0530

    Update programs.py

--- programs.py
+++ programs.py
@@ -10,5 +10,35 @@ for i in range(1, num + 1):
 print("factorial of ", num, " is ", fac)
 
 # Write a python script to construct the following pattern
+# (a) 
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * *
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
+# (b)
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
 
+for i in range(rows, 0, -1):
+    for j in range(0, i - 1):
+        print("*", end=' ')
+    print("\r")
                                                              
