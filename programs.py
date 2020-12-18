99fdf493c539a2e0e39f6771f68b814e2b60bf61
AZHAR SAYYED <55562412+Azharsayyed5@users.noreply.github.com>
2020-12-18 20:22:36 +0530

    Create programs.py

---
+++ programs.py
@@ -0,0 +1,14 @@
+# Write a program to calculate and print the factorial of a number using a for loop.
+
+num = int(input("enter a number: "))
+ 
+fac = 1
+ 
+for i in range(1, num + 1):
+	fac = fac * i
+ 
+print("factorial of ", num, " is ", fac)
+
+# Write a python script to construct the following pattern
+
+                                                             
